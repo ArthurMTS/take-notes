e54505b46fc9cf1ba0344d8ef426b83c8b5dc608
Arthur Silva <arthurmatsilva@outlook.com>
2023-03-06 15:55:59 -0300

    fix: data atrasada em 1 dia

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -18,6 +18,6 @@ export const getWeekendDate = () => {
 export const formatedDate = (dateString: string) => {
   const date = new Date(dateString);
   const meses = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul","Ago","Set","Out","Nov","Dez"];
-  const formatedDate = ((date.getDate() + " " + meses[(date.getMonth())] + " " + date.getFullYear()));
+  const formatedDate = ((String(+date.getDate() + 1) + " " + meses[(date.getMonth())] + " " + date.getFullYear()));
   return formatedDate;
 };
